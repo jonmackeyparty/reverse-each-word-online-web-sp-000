d8271d0a85dd931767ee4c39200bae51b6cc8330
Jon Mackey <jon@jonmackey.party>
2018-11-15 01:53:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   array = sentence.split
   new_array = []
-  array.each{|x| new_array << x.reverse}
+  array.each{|x| new_array << x.reverse.join}
   new_array
 end 
